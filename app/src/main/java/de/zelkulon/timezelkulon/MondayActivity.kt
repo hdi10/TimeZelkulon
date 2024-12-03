c39443c324151dcab7194ec23d69cae40b05a6da
Harun Dastekin <hdastekin@gmail.com>
2024-12-03 14:34:50 +0100

    work on using imageFrame

--- app/src/main/java/de/zelkulon/timezelkulon/MondayActivity.kt
+++ app/src/main/java/de/zelkulon/timezelkulon/MondayActivity.kt
@@ -3,7 +3,7 @@ package de.zelkulon.timezelkulon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -15,6 +15,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 
 class MondayActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,5 +48,9 @@ fun MainMondayContent() {
 fun MondayContent(modifier: Modifier = Modifier) {
     Column(modifier) {
         Text(text = "Hier Montagsdaten")
+
+        Image(painter = painterResource(id = R.drawable.dog_51509), contentDescription = "Picture of a pit bull or pug breed")
+
+        val imageBitmap = ImageBitmap.imageResource(R.drawable.dog_51509)
     }
 }
--- app/src/main/java/de/zelkulon/timezelkulon/ThursdayActivity.kt
+++ app/src/main/java/de/zelkulon/timezelkulon/ThursdayActivity.kt
@@ -3,6 +3,7 @@ package de.zelkulon.timezelkulon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.findViewTreeLifecycleOwner
 
 
 class ThursdayActivity : ComponentActivity() {
@@ -44,9 +51,29 @@ fun MainThursdayContent() {
     )
 }
 
+
+
 @Composable
 fun ThursdayContent(modifier: Modifier = Modifier) {
     Column(modifier) {
         Text(text = "Hier Daten vom Donnerstag")
     }
 }
+
+@Preview
+@Composable
+fun ImageBitmapSnippets() {
+    // [START android_compose_images_bitmap_load]
+    Image(
+        painter = painterResource(id = R.drawable.dog_51509),
+        contentDescription = stringResource(id = R.string.dog_content_description)
+    )
+    // [END android_compose_images_bitmap_load]
+
+    // [START android_compose_images_bitmap_simple]
+    val imageBitmap = ImageBitmap.imageResource(R.drawable.dog_51509)
+    // [END android_compose_images_bitmap_simple]
+}
+
+
+
